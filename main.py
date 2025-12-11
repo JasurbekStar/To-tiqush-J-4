f326899b0bfe0293c3bdecf03a77ac42a4463ea5
JasurbekStar <156973763+JasurbekStar@users.noreply.github.com>
2025-12-11 14:51:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import Message
+
+API = "8518648084:AAHorbcdwWGBbTqEBjlZGVGOK2dmN1PICxQ"
+
+dp = Dispatcher()
+@dp.message(Command('start'))
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Salom, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    await message.answer(f"{message.text}")
+
+
+async def main() -> None:
+    bot = Bot(token=API, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+if name == "main":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
